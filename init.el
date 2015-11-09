4aa43a2b7be87a3ac357e6713377421637c67ee6
risou <risou.f@gmail.com>
2015-11-09 18:31:55 +0900

    set php indent style

--- init.el
+++ init.el
@@ -578,7 +578,17 @@
 						ac-source-words-in-same-mode-buffers
 						ac-source-php-completion
 						ac-source-filename
-						))))
+						))
+	(defun ywb-php-lineup-arglist-intro (langelem)
+	  (save-excursion
+		(goto-char (cdr langelem))
+		(vector (+ (current-column) c-basic-offset))))
+	(defun ywb-php-lineup-arglist-close (langelem)
+	  (save-excursion
+		(goto-char (cdr langelem))
+		(vector (current-column))))
+	(c-set-offset 'arglist-intro 'ywb-php-lineup-arglist-intro)
+	(c-set-offset 'arglist-close 'ywb-php-lineup-arglist-close)))
 
 ;; gtags
 ;; curl -O http://tamacom.com/global/global-6.1.tar.gz
