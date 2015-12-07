96f80df9750e187f854019d55f59c348b8f47d42
risou <risou.f@gmail.com>
2015-12-07 18:38:58 +0900

    synclonize Emacs kill-ring and Mac OS X clipboard

--- init.el
+++ init.el
@@ -162,6 +162,18 @@
   (command-execute 'isearch-forward))
 (global-set-key "\C-s" 'isearch-forward-with-heading)
 
+;; クリップボード同期
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+	(let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+	  (process-send-string proc text)
+	  (process-send-eof proc))))
+(when (eq system-type 'darwin)
+  (setq interprogram-cut-function 'paste-to-osx)
+  (setq interprogram-paste-function 'copy-from-osx))
+
 ;; Window分割
 ;; prefixをC-zに
 ;; (define-key global-map "\C-z" (make-sparse-keymap))
